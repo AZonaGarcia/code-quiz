365b8c1ee4588b87f70d469788b5978ef377b591
Bonus Mom <AZonaGarcia@aol.com>
2020-08-06 22:08:34 -0400

    created file structure

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var questionsArr = [
+ {
+    questions: "", 
+    answers: [],
+    correctIndex: 1
+},
+
+{
+    questions: "", 
+    answers: [],
+    correctIndex: 2
+},
+
+{
+    questions: "", 
+    answers: [],
+    correctIndex: 3
+}
+
+]
